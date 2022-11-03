89ba854c2b9c9c104e1cddb4a990b3f01229eb82
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-11-03 13:28:27 +0100

    delete unused file from script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,4 @@
 import {showMobileMenu, closeMobileMenu} from './menu.js';
-import {changeBorderColor} from './border.js';
-import {validate} from './formValidator.js';
 
 showMobileMenu();
 closeMobileMenu();
